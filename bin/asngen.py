c296665cc33d1d9535eb2b527cfd078e9503c469
Xtina S <xtina.schelin@gmail.com>
2019-01-06 17:43:45 -0500

    Fixing file path, exceptions, CSV handling.

--- bin/asngen.py
+++ bin/asngen.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 
-from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option, validators
-import sys
-import os
 import ConfigParser
-from StringIO import StringIO
-from zipfile import ZipFile
-import urllib2
+import csv
+import os
 import re
 import socket
+from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option, validators
+from StringIO import StringIO
 import struct
+import sys
+import urllib2
+from zipfile import ZipFile
 
 @Configuration(type='reporting')
 class ASNGenCommand(GeneratingCommand):
 
     def generate(self):
-                                                      
         proxies = {'http': None, 'https': None}
 
+        # Retrieve proxy settings, or skip.
         try:
             configparser = ConfigParser.ConfigParser()
             configparser.read(os.path.join(os.environ['SPLUNK_HOME'], 'etc/apps/TA-asngen/local/asngen.conf'))
@@ -38,86 +39,29 @@ class ASNGenCommand(GeneratingCommand):
             opener = urllib2.build_opener(proxy)
             urllib2.install_opener(opener)
 
+        # Attempt to retrieve the file.
         try:
-            url = urllib2.urlopen("https://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum2.zip")
-        except:
-            raise Exception("Please check app proxy settings")
+            url = urllib2.urlopen("https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip")
+        except Exception as ex:
+            raise Exception(ex)
 
-        if url.getcode()==200:
+        # TODO: Is this necessary, given the above? Seems the previous try/except catches when it's a 404...
+        if url.getcode() == 200:
             try:
                 zipfile = ZipFile(StringIO(url.read()))
-            except:
-                raise Exception("Invalid zip file")
+            except Exception as ex:
+                raise Exception(ex)
         else:
             raise Exception("Received response: " + url.getcode())
 
+        # Get the entries from each CSV file in the ZIP file.
+        pate = r'^([0-9\.\/]+),(\d+),\"?(([^\"]+)|)\"?'
         for name in zipfile.namelist():
-            entries = re.findall(r'^(\d+),(\d+),\"AS(\d+) ([^\"]+)\"', zipfile.open(name).read(), re.MULTILINE)
-
-        for line in entries:
-            diff = (int(line[1]) - int(line[0])) -1
-            ip = socket.inet_ntoa(struct.pack('!L', int(line[0])))
-
-            # there's almost certainly a more elegant way of doing this but it works
-            if diff<2**2:
-                mask = 30
-            elif diff<2**3:
-                mask = 29
-            elif diff<2**4:
-                mask = 28
-            elif diff<2**5:
-                mask = 27
-            elif diff<2**6:
-                mask = 26
-            elif diff<2**7:
-                mask = 25
-            elif diff<2**8:
-                mask = 24
-            elif diff<2**9:
-                mask = 23
-            elif diff<2**10:
-                mask = 22
-            elif diff<2**11:
-                mask = 21
-            elif diff<2**12:
-                mask = 20
-            elif diff<2**13:
-                mask = 19
-            elif diff<2**14:
-                mask = 18
-            elif diff<2**15:
-                mask = 17
-            elif diff<2**16:
-                mask = 16
-            elif diff<2**17:
-                mask = 15
-            elif diff<2**18:
-                mask = 14
-            elif diff<2**19:
-                mask = 13
-            elif diff<2**20:
-                mask = 12
-            elif diff<2**21:
-                mask = 11
-            elif diff<2**22:
-                mask = 10
-            elif diff<2**23:
-                mask = 9
-            elif diff<2**24:
-                mask = 8
-            elif diff<2**25:
-                mask = 7
-            elif diff<2**26:
-                mask = 6
-            elif diff<2**27:
-                mask = 5
-            elif diff<2**28:
-                mask = 4
-            elif diff<2**29:
-                mask = 3
-            elif diff<2**30:
-                mask = 2
-
-            yield {'ip': ip + "/" + str(mask), 'start': line[0], 'end': line[1], 'asn': line[2], 'autonomous_system': line[3].decode('utf-8', 'ignore')}
+            if os.path.splitext(name)[1] == ".csv":
+                with zipfile.open(name) as csvname:
+                    csv_reader = csv.reader(csvname, delimiter=',', quotechar='"')
+                    for row in csv_reader:
+                        if re.match(pate, ",".join(row)) is not None:
+                            yield {'ip': line[0], 'asn': line[1], 'autonomous_system': line[2].decode('utf-8', 'ignore')}
 
 dispatch(ASNGenCommand, sys.argv, sys.stdin, sys.stdout, __name__)
